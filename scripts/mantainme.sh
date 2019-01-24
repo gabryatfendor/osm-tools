a45be805ba3e03ccd0ec80d0f22d994a8b7f9596
gabryatfendor <gabryatfendor@gmail.com>
2019-01-24 14:49:35 +0000

    Finished timetosurvey.sh Now correctly using day parameter

--- scripts/mantainme.sh
+++ scripts/mantainme.sh
@@ -14,7 +14,7 @@ if [ $# -lt 3 ]
 fi
 
 AREA=$1
-OUTPUT_FILENAME=$2
+OUTPUT_FILENAME=$2.html
 OUTPUT_PATH=$3
 
 AREACODE_QUERY=`encode_url_string "area[\"boundary\"=\"administrative\"][\"name\"=\"$AREA\"];out ids;"`
@@ -35,11 +35,13 @@ wget -nc -O $AREA.csv $OVERPASS_API_URL$CSV_QUERY
 
 awk -F ";" 'FNR==1 {print $0} FNR > 1 { print "<a href=\"https://openstreetmap.org/relation/"$1"\">"$1"</a>",";"$2,";"$3,";"$4,";"$5 }' $AREA.csv > $AREA.tmp
 
-echo "<h3>List of tracks in $AREA that needs maintenance<br><br>" > $OUTPUT_FILENAME.html
-echo "<style>table, th, td { border: 1px solid black; border-collapse: collapse; }</style>" >> $OUTPUT_FILENAME.html
-echo "<table><tr><th>Relation</th><th>Name</th><th>Ref Number</th><th>Maintenance</th><th>Maintenance:it</th></tr>" >> $OUTPUT_FILENAME.html
+echo "<h3>List of tracks in $AREA that needs maintenance</h3>" > $OUTPUT_FILENAME
+echo "<style>table, th, td { border: 1px solid black; border-collapse: collapse; }</style>" >> $OUTPUT_FILENAME
+echo "<table><tr><th>Relation</th><th>Name</th><th>Ref Number</th><th>Maintenance</th><th>Maintenance:it</th></tr>" >> $OUTPUT_FILENAME
+awk -F ";" ' NR>1 { print "<tr><td>"$1"</td><td>"$2"</td><td>"$3"</td><td>"$4"</td><td>"$5"</td></tr>"} ' $AREA.tmp >> $OUTPUT_FILENAME
+echo "</table>" >> $OUTPUT_FILENAME
 
-awk -F ";" ' NR>1 { print "<tr><td>"$1"</td><td>"$2"</td><td>"$3"</td><td>"$4"</td><td>"$5"</td></tr>"} ' $AREA.tmp >> $OUTPUT_FILENAME.html
+echo "<br><br><footer>Generated on `date`</footer>" >> $OUTPUT_FILENAME
 
 if [ "$4" != "--keep" ]
 	then
@@ -48,4 +50,4 @@ if [ "$4" != "--keep" ]
 		rm $AREA.tmp
 fi
 
-mv $OUTPUT_FILENAME.html $OUTPUT_PATH
+mv $OUTPUT_FILENAME $OUTPUT_PATH
--- scripts/timetosurvey.sh
+++ scripts/timetosurvey.sh
@@ -14,8 +14,13 @@ fi
 
 AREA=$1
 SURVEY_INTERVAL=$2
-OUTPUT_FILENAME=$3
+OUTPUT_FILENAME=$3.html
 OUTPUT_PATH=$4
+
+echo "Checking for trail that were not surveyed in the last $SURVEY_INTERVAL days"
+LIMIT_DATE=`date --date="$SURVEY_INTERVAL day ago" +%Y-%m-%d`
+
+echo "So anything that was not surveyed after $LIMIT_DATE"
 
 AREACODE_QUERY=`encode_url_string "area[\"boundary\"=\"administrative\"][\"name\"=\"$AREA\"];out ids;"`
 AREACODE=`curl -s $OVERPASS_API_URL$AREACODE_QUERY | grep 3600 | awk -F "\"" '{print $2}'`
@@ -27,22 +32,29 @@ if [ -z $AREACODE ]
 fi
 
 echo "Input area is $AREA"
-echo "Related code is $AREACODE"
 
 CSV_QUERY=`encode_url_string "[out:csv(::id,\"name\",\"ref\",\"survey:date\";true;\";\")];area[\"name\"=\"$AREA\"]->.a;relation[\"operator\"~\"CAI\"](area.a);out;"`
 
-wget -nc -O $AREA.csv $OVERPASS_API_URL$CSV_QUERY
+echo "Downloading query result..."
+
+wget -q -nc -O $AREA.csv $OVERPASS_API_URL$CSV_QUERY
 
 awk -F ";" 'FNR==1 {print $0} FNR > 1 { print "<a href=\"https://openstreetmap.org/relation/"$1"\">"$1"</a>",";"$2,";"$3,";"$4 }' $AREA.csv > $AREA.tmp
 
-echo "<h3>List of tracks in $AREA that needs to be surveyed<br><br>" > $OUTPUT_FILENAME.html
-echo "<style>table, th, td { border: 1px solid black; border-collapse: collapse;} .urgent {color: red; font-weight: bold;}</style>" >> $OUTPUT_FILENAME.html
-echo "<table><tr><th>Relation</th><th>Name</th><th>Ref Number</th><th>Last Known Survey Date</th></tr>" >> $OUTPUT_FILENAME.html
+echo "<h3>List of tracks in $AREA that needs to be surveyed</h3><br><br>" > $OUTPUT_FILENAME
+echo "These tracks were not surveyed in the past $SURVEY_INTERVAL days, so they were all surveyed prior to $LIMIT_DATE<br>" >> $OUTPUT_FILENAME
+echo "<style>table, th, td { border: 1px solid black; border-collapse: collapse;} .urgent {color: red; font-weight: bold;}</style>" >> $OUTPUT_FILENAME
+echo "<table><tr><th>Relation</th><th>Name</th><th>Ref Number</th><th>Last Known Survey Date</th></tr>" >> $OUTPUT_FILENAME
 
-awk -F ";" '{ if (NR>1 && $4!="") print "<tr><td>"$1"</td><td>"$2"</td><td>"$3"</td><td>"$4"</td></tr>"; else if (NR>1 && $4=="") print "<tr><td>"$1"</td><td>"$2"</td><td>"$3"</td><td class=\"urgent\">No last known survey date!</td></tr>"; } ' $AREA.tmp >> $OUTPUT_FILENAME.html
+awk -v limit_date="$LIMIT_DATE" -F ";" '{ if (NR>1 && $4!="" && $4 <= limit_date) print "<tr><td>"$1"</td><td>"$2"</td><td>"$3"</td><td>"$4"</td></tr>"; else if (NR>1 && $4=="") print "<tr><td>"$1"</td><td>"$2"</td><td>"$3"</td><td class=\"urgent\">Never surveyed!</td></tr>"; } ' $AREA.tmp >> $OUTPUT_FILENAME
+
+echo "</table>" >> $OUTPUT_FILENAME
+
+
+echo "<br><br>Generated on `date`" >> $OUTPUT_FILENAME
 
 echo "Removing temp file"
 rm $AREA.csv
 rm $AREA.tmp
 
-mv $OUTPUT_FILENAME.html $OUTPUT_PATH
+mv $OUTPUT_FILENAME $OUTPUT_PATH
